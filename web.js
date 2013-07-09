ad990dcfa9f772be74239bd61622c62b95afc939
maldharm <dharma.mala85@gmail.com>
2013-07-09 21:29:55 +0000

    add

--- web.js
+++ web.js
@@ -1,6 +1,4 @@
 var express = require('express');
-<<<<<<< HEAD
-<<<<<<< HEAD
 var fs = require('fs');
 var app = express.createServer(express.logger());
 
@@ -8,29 +6,9 @@ app.get('/', function(request, response) {
   var buffer = new Buffer(24);
   buffer = fs.readFileSync('index.html');
   response.send(buffer.toString("utf-8",0,buffer.length));
-=======
-=======
->>>>>>> 90fd58e343c29e54fc1d4e19036595500b2bdc21
-
-var app = express.createServer(express.logger());
-
-app.get('/', function(request, response) {
-  response.send('Hello World!');
-<<<<<<< HEAD
->>>>>>> 90fd58e343c29e54fc1d4e19036595500b2bdc21
-=======
->>>>>>> 90fd58e343c29e54fc1d4e19036595500b2bdc21
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
-<<<<<<< HEAD
-<<<<<<< HEAD
 });
-=======
-});
->>>>>>> 90fd58e343c29e54fc1d4e19036595500b2bdc21
-=======
-});
->>>>>>> 90fd58e343c29e54fc1d4e19036595500b2bdc21
