40492e1d63570b22e864050547d01513159f7c73
maldharm <dharma.mala85@gmail.com>
2013-07-10 19:30:43 +0000

    added

--- web.js
+++ web.js
@@ -3,8 +3,7 @@ var fs = require('fs');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var buffer = new Buffer(200000);
-  buffer = fs.readFileSync('index.html');
+  var buffer = fs.readFileSync('index.html');
   response.send(buffer.toString("utf-8",0,buffer.length));
 });
 
